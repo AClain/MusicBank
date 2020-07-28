96dd06c444e33d5b5db6662c69c1218c686b25c1
Alexandre Clain <alexandre.clain11@gmail.com>
2020-07-29 01:16:08 +0200

    Update starting_db.sql

--- api/starting_db.sql
+++ api/starting_db.sql
@@ -19,9 +19,9 @@
 -- Current Database: `music`
 --
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `music` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `musicbank` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
-USE `music`;
+USE `musicbank`;
 
 --
 -- Table structure for table `album`
